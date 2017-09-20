10efdc98b26ffe4429d4ab951a330ea65c2c1133
rrandom <emanonhere@gmail.com>
2017-09-20 23:13:02 +0800

    zoom

---
+++ src/components/Zoom.vue
@@ -0,0 +1,53 @@
+<template>
+  <svg :width="width" :height="height">
+    <g :transform="transform">
+      <circle v-for="(point, key) in points" :cx="point.x" :cy="point.y" :r="2.5" :key="key"></circle>
+    </g>
+  </svg>
+</template>
+<script>
+
+/**
+ * Drag & Zoom II from https://bl.ocks.org/mbostock/3127661b6f13f9316be745e77fdfb084
+*/
+
+import * as d3 from 'd3';
+
+export default {
+  name: 'zoom',
+  data() {
+    return {
+      width: 960,
+      height: 500,
+      transform: '',
+      zoom: d3.zoom().scaleExtent([1 / 2, 8]).on('zoom', this.handleZoom),
+      points: [],
+    };
+  },
+  methods: {
+    handleZoom() {
+      this.transform = d3.event.transform;
+    },
+    phyllotaxis(radius) {
+      const theta = Math.PI * (3 - Math.sqrt(5));
+      return (i) => {
+        const r = radius * Math.sqrt(i);
+        const a = theta * i;
+        return {
+          x: this.width / 2 + r * Math.cos(a),
+          y: this.height / 2 + r * Math.sin(a),
+        };
+      };
+    },
+    initPoints() {
+      this.points = d3.range(2000).map(this.phyllotaxis(10));
+    },
+  },
+  mounted() {
+    this.initPoints();
+    this.wrapper = d3.select(this.$el);
+    this.zoom(this.wrapper);
+  },
+};
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import Zoom from '@/components/Zoom';
 
 Vue.use(Router);
 
@@ -9,6 +10,16 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
+      component: Hello,
+    },
+    {
+      path: '/zoom',
+      name: 'Zoom',
+      component: Zoom,
+    },
+    {
+      path: '*',
+      name: '404',
       component: Hello,
     },
   ],
